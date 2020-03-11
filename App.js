a2830441dc44639fcd331564d76cecce76490045
abduljeleelng <abduljeleelng@gmail.com>
2020-03-11 10:15:56 +0100

    Comment

--- App.js
+++ App.js
@@ -4,12 +4,93 @@ import Constants from 'expo-constants';
 import React from 'react';
 
 import Feed from './screens/Feed';
+import Comments from './screens/Comments'
+
+const ASYNC_STORAGE_COMMENTS_KEY = 'ASYNC_STORAGE_COMMENTS_KEY';
 
 export default class App extends React.Component {
+  state = {
+    commentsForItem: {},
+    showModal: false,
+    selectedItemId: null,
+  };
+
+  async componentDidMount() {
+    try {
+      const commentsForItem = await AsyncStorage.getItem(
+        ASYNC_STORAGE_COMMENTS_KEY,
+      );
+
+      this.setState({
+        commentsForItem: commentsForItem
+          ? JSON.parse(commentsForItem)
+          : {},
+      });
+    } catch (e) {
+      console.log('Failed to load comments');
+    }
+  }
+
+  onSubmitComment = async text => {
+    const { selectedItemId, commentsForItem } = this.state;
+    const comments = commentsForItem[selectedItemId] || [];
+
+    const updated = {
+      ...commentsForItem,
+      [selectedItemId]: [...comments, text],
+    };
+
+    this.setState({ commentsForItem: updated });
+
+    try {
+      await AsyncStorage.setItem(
+        ASYNC_STORAGE_COMMENTS_KEY,
+        JSON.stringify(updated),
+      );
+    } catch (e) {
+      console.log(
+        'Failed to save comment',
+        text,
+        'for',
+        selectedItemId,
+      );
+    }
+  };
+
+  openCommentScreen = id => {
+    this.setState({
+      showModal: true,
+      selectedItemId: id,
+    });
+  };
+
+  closeCommentScreen = () => {
+    this.setState({
+      showModal: false,
+      selectedItemId: null,
+    });
+  };
   render() {
+    const { commentsForItem, showModal, selectedItemId } = this.state;
     return (
       <View style={styles.container}>
-        <Feed style={styles.feed} />
+        <Feed
+          style={styles.feed}
+          commentsForItem={commentsForItem}
+          onPressComments={this.openCommentScreen}
+        />
+        <Modal
+          visible={showModal}
+          animationType="slide"
+          onRequestClose={this.closeCommentScreen}
+        >
+          <Comments
+            style={styles.comments}
+            comments={commentsForItem[selectedItemId] || []}
+            onClose={this.closeCommentScreen}
+            onSubmitComment={this.onSubmitComment}
+          />
+        </Modal>
       </View>
     );
   }
@@ -30,7 +111,15 @@ const styles = StyleSheet.create({
     marginTop:
       Platform.OS === 'android' || platformVersion < 11
         ? Constants.statusBarHeight
-        : 10,
+        : 0,
+  },
+  comments: {
+    flex: 1,
+    marginTop:
+      Platform.OS === 'ios' && platformVersion < 11
+        ? Constants.statusBarHeight
+        : 0,
   },
 });
 //Constants.statusBarHeight
+
